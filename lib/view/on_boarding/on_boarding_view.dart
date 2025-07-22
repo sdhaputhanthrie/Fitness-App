dd8723f15360cf6c9355ac86ee84fdd812a9a227
sdhaputhanthrie <sanadeehaputhanthire@gmail.com>
2025-07-22 17:25:16 +0530

    create on boarding page

--- lib/view/on_boarding/on_boarding_view.dart
+++ lib/view/on_boarding/on_boarding_view.dart
@@ -1,3 +1,4 @@
+import 'package:fitness/common_widget/on_boarding_page.dart';
 import 'package:flutter/material.dart';
 
 import '../../common/colo_extension.dart';
@@ -12,6 +13,33 @@ class OnBoardingView extends StatefulWidget {
 class _OnBoardingViewState extends State<OnBoardingView> {
   PageController controller = PageController();
 
+  List pageArr = [
+    {
+      "title": "Track your Goal",
+      "subtitle":
+          "Don't worry if you have trouble determininag your goals, We can help you determine your goals and track your goals",
+      "image": "assets/img/co (1).png",
+    },
+    {
+      "title": "Get Burn",
+      "subtitle":
+          "Let's keep burning, to achive yours goals, it hurts only teporarily, if you give up now you will be in pain forever",
+      "image": "assets/img/co (2).png",
+    },
+    {
+      "title": "Eat Well",
+      "subtitle":
+          "Let's start healthy lifestyle with us, we can determine your diet every day. healthy eating is fun",
+      "image": "assets/img/co (3).png",
+    },
+    {
+      "title": "Improve Sleep\nQuality",
+      "subtitle":
+          "Improve the quality of your sleep with us, good quality sleep can bring a good mood in the morning",
+      "image": "assets/img/co (4).png",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
@@ -19,25 +47,25 @@ class _OnBoardingViewState extends State<OnBoardingView> {
     return Scaffold(
       backgroundColor: TColor.white,
       body: Stack(
+        alignment: Alignment.bottomRight,
         children: [
           PageView.builder(
             controller: controller,
-            itemCount: 3,
+            itemCount: pageArr.length,
             itemBuilder: (context, index) {
+              var pObj = pageArr[index] as Map? ?? {};
+              return OnBoardingPage(pObj: pObj);
+            }
+          ),
 
-              return SizedBox(
-                width:media.width,
-                height: media.height,
-                child: Column(
-                   mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(height: 30), 
-                  Image.asset("assets/img/co (1).png", width: media.width , fit:BoxFit.fitWidth, )
-                 ],
-                ),
-              );
-            },)
-          
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            color: Colors.blue,
+            child: IconButton(icon: Icon(Icons.navigate_next, color: TColor.white,),color: TColor.primaryColor1 , onPressed: (){
+
+            },
+          ),
+          )
         ],
       ),
     );
