73675d5236b5156d979b54a3fd5557f548b8a842
sdhaputhanthrie <sanadeehaputhanthire@gmail.com>
2025-07-25 00:11:51 +0530

    round button file update

---
+++ lib/common_widget/round_buttom.dart
@@ -0,0 +1,57 @@
+import 'package:fitness/common/colo_extension.dart';
+import 'package:flutter/material.dart';
+
+enum RoundButtonType { bgGradient, textGradient }
+  
+
+class RoundButtom extends StatelessWidget {
+  final String title;
+  final RoundButtonType type;
+  final VoidCallback onPressed;
+  const RoundButtom({super.key, required this.title, this.type =RoundButtonType.textGradient, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      height: 50,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+      textColor: TColor.primaryColor1,
+      color: TColor.white,
+      minWidth: double.maxFinite,
+
+      child:Ink(
+        width: double.maxFinite,
+        height: 50,
+        decoration:  BoxDecoration(
+          gradient:LinearGradient(
+            colors: TColor.primaryG,
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight) ,
+          borderRadius: const BorderRadius.all(Radius.circular(25.0)),
+        ),
+      
+      child: 
+      
+      //ShaderMask(
+        //blendMode: BlendMode.srcIn, 
+        //shaderCallback: (bounds) {
+          //return LinearGradient(
+           // colors: TColor.primaryG,
+           // begin: Alignment.centerLeft,
+           // end: Alignment.centerRight)
+          //.createShader(Rect.fromLTRB(0, 0, bounds.width, bounds.height));
+       // },
+        //child: 
+        
+        Text(title,
+           
+          style: TextStyle(
+            color: TColor.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w700)),
+          
+     // ),
+  ));
+  }
+}
