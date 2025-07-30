bbcac4d26953284741045e0df9106602eb7411e1
sdhaputhanthrie <sanadeehaputhanthire@gmail.com>
2025-07-30 22:09:36 +0530

    add date and weight

--- lib/view/login/complete_profile_view.dart
+++ lib/view/login/complete_profile_view.dart
@@ -1,5 +1,5 @@
-
 import 'package:fitness/common/colo_extension.dart' show TColor;
+import 'package:fitness/common_widget/round_textfield.dart';
 import 'package:flutter/material.dart';
 
 class CompleteProfileView extends StatefulWidget {
@@ -10,9 +10,10 @@ class CompleteProfileView extends StatefulWidget {
 }
 
 class _CompleteProfileViewState extends State<CompleteProfileView> {
+  TextEditingController txtDate = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-
     var media = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: TColor.white,
@@ -20,13 +21,60 @@ class _CompleteProfileViewState extends State<CompleteProfileView> {
         child: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Column(children: [
-             Image.asset("assets/img/gym.png",width: media.width ,fit:BoxFit.fitWidth) 
-            ],),
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/img/gym.png",
+                  width: media.width,
+                  fit: BoxFit.fitWidth,
+                  height: 350,
+                ),
+
+                Text(
+                  "Let's complete your profile",
+                  style: TextStyle(color: TColor.black, fontSize: 20, fontWeight: FontWeight.w700,),
+                ),
+                
+                Text(
+                  "It will help us to know more about you!",
+                  style: TextStyle(
+                    color: TColor.gray,
+                    fontSize: 16,
+                    
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Column(
+                    children: [
+                      RoundTextfield(
+                        controller: txtDate,
+                        hitText: "Date of Birth",
+                        icon: "assets/img/date.png",
+                      ),
+
+                      Row(
+                        children: [
+                        Expanded(
+                          child: RoundTextfield(
+                          controller: txtDate,
+                          hitText: "Your weight",
+                          icon: "assets/img/weight.png",
+                                                ),
+                        ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+
+                SizedBox(height: media.height * 0.05),
+              ],
+            ),
           ),
         ),
       ),
     );
-    
   }
 }
