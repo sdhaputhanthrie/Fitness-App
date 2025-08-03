3f6779cbf3795cc7c76de2100fbf504b47458eed
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2025-08-03 12:35:41 +0530

    change welcome text

---
+++ lib/view/login/welcome_view.dart
@@ -0,0 +1,67 @@
+
+import 'package:fitness/common/colo_extension.dart';
+import 'package:flutter/material.dart';
+
+import '../../common_widget/round_button.dart';
+
+class WelcomeView extends StatefulWidget {
+  const WelcomeView({super.key});
+
+  @override
+  State<WelcomeView> createState() => _WelcomeViewState();
+}
+
+class _WelcomeViewState extends State<WelcomeView> {
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(backgroundColor:TColor.white,
+    body: SafeArea(
+      child: Container(width: media.width,
+       padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 25),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        SizedBox(height: media.height * 0.01),
+        Image.asset(
+                  "assets/img/welcome.png",
+                  width: media.width * 0.8,
+                  fit: BoxFit.fitWidth,
+                  height: 350,
+                ),
+
+                Text(
+                  "Welcome Stefani",
+                  style: TextStyle(color: TColor.black, fontSize: 20, fontWeight: FontWeight.w700,),
+                ),
+                
+                Text(
+                  "You are all set now,let's reach your \n together with us ",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: TColor.gray,
+                    fontSize: 16,
+                    
+                  ),
+                ),
+                const Spacer(),
+
+                  RoundButton(
+                    title: "Go to Home",
+                    onPressed: () {
+                      //Navigator.push(
+                        //context,
+                        //MaterialPageRoute(
+                          //builder: (context) => const WelcomeView(),
+                        //),
+                      //);
+                    },
+                  ),
+                SizedBox(height: media.height * 0.1),
+      ],
+    ),
+    
+    )),);
+  }
+}
