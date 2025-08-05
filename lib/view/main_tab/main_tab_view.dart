382820f4b61a81342eb8ffe56ed58d7348c4420f
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2025-08-05 23:26:06 +0530

    add new path

--- lib/view/main_tab/main_tab_view.dart
+++ lib/view/main_tab/main_tab_view.dart
@@ -1,5 +1,7 @@
 import 'package:fitness/common/colo_extension.dart';
 import 'package:fitness/common_widget/tab_button.dart';
+import 'package:fitness/view/home/blank_view.dart';
+import 'package:fitness/view/home/home_view.dart';
 import 'package:flutter/material.dart';
 
 class MainTabView extends StatefulWidget {
@@ -11,64 +13,99 @@ class MainTabView extends StatefulWidget {
 
 class _MainTabViewState extends State<MainTabView> {
   int selectTab = 0;
+  final PageStorageBucket pageBucket = PageStorageBucket();
+  Widget currentTab = const HomeView(); 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: TColor.white,
+      body: PageStorage(bucket: pageBucket, child: currentTab),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: SizedBox(width: 70,height: 70,child: InkWell(onTap: (){},child: Container(
-       
+      floatingActionButton: SizedBox(
+        width: 70,
+        height: 70,
+        child: InkWell(
+          onTap: () {},
+          child: Container(
             width: 50,
             height: 50,
             decoration: BoxDecoration(
-              gradient: LinearGradient(
-              colors: TColor.primaryG,),
+              gradient: LinearGradient(colors: TColor.primaryG),
               borderRadius: BorderRadius.circular(35),
-              boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 2,)
-            ]),
-            
-            child: Icon(Icons.search,color: TColor.white,size: 35,),
-          
-      ),),),
-      bottomNavigationBar: BottomAppBar(
-       
-          child: Container(
-            decoration: BoxDecoration(color: TColor.white, boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 2, offset: Offset(0, -2))
-            ]),
-            height: kToolbarHeight,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                
-                    TabButton(icon: "assets/img/home_tab.png", selectIcon: "assets/img/home_tab.png", isActive: selectTab == 0, onTap:(){
-                      selectTab = 0;
-                      if(mounted)
-                      setState(() {});
-                    }),
-
-                    TabButton(icon: "assets/img/activity_tab.png", selectIcon: "assets/img/activity_tab.png", isActive: selectTab == 1, onTap:(){
-                      selectTab = 1;
-                      if(mounted)
-                      setState(() {});
-                    }),
-                    const SizedBox(width: 40,),
-
-                    TabButton(icon: "assets/img/camera_tab.png", selectIcon: "assets/img/camera_tab.png", isActive: selectTab == 2, onTap:(){
-                      selectTab = 2;
-                      if(mounted)
-                      setState(() {});
-                    }),
-
-                     TabButton(icon: "assets/img/profile_tab.png", selectIcon: "assets/img/profile_tab.png", isActive: selectTab == 3, onTap:(){
-                      selectTab = 3;
-                      if(mounted)
-                      setState(() {});
-                    }),
-
-
+              boxShadow: const [
+                BoxShadow(color: Colors.black12, blurRadius: 2),
               ],
             ),
-          )),
-      );
+
+            child: Icon(Icons.search, color: TColor.white, size: 35),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          decoration: BoxDecoration(
+            color: TColor.white,
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 2,
+                offset: Offset(0, -2),
+              ),
+            ],
+          ),
+          height: kToolbarHeight,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TabButton(
+                icon: "assets/img/home_tab.png",
+                selectIcon: "assets/img/home_tab.png",
+                isActive: selectTab == 0,
+                onTap: () {
+                  selectTab = 0;
+                   currentTab = const HomeView();
+                  if (mounted) setState(() {});
+                },
+              ),
+
+              TabButton(
+                icon: "assets/img/activity_tab.png",
+                selectIcon: "assets/img/activity_tab.png",
+                isActive: selectTab == 1,
+                onTap: () {
+                  selectTab = 1;
+                  currentTab = const BlankView();
+                  if (mounted) setState(() {});
+                },
+              ),
+              const SizedBox(width: 40),
+
+              TabButton(
+                icon: "assets/img/camera_tab.png",
+                selectIcon: "assets/img/camera_tab.png",
+                isActive: selectTab == 2,
+                onTap: () {
+                  selectTab = 2;
+                  currentTab = const BlankView();
+                  if (mounted) setState(() {});
+                },
+              ),
+
+              TabButton(
+                icon: "assets/img/profile_tab.png",
+                selectIcon: "assets/img/profile_tab.png",
+                isActive: selectTab == 3,
+                onTap: () {
+                  selectTab = 3;
+                  currentTab = const BlankView();
+                  if (mounted) setState(() {});
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-  }
+}
