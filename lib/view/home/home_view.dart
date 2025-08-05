6df8a263ec6705ba55878a00e883804193453349
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2025-08-05 23:25:31 +0530

    add new icon and titles

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -12,8 +12,58 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: TColor.white,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              children: [
+            
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            
+                  children: [
+                 
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      
+            
+              children: [
+
+                SizedBox(height: media.height * 0.02),
+                 Text(
+                    "Welcome Back",
+                    style: TextStyle(color: TColor.gray, fontSize: 16),
+                  ),
+                  Text(
+                    "Stefani wong ",
+                    style: TextStyle(
+                      color: TColor.black,
+                      fontSize:20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+            
+                  SizedBox(height: media.height * 0.02),
+              ],
+            ),
+            IconButton(onPressed: (){
+
+            }, icon: Image.asset("assets/img/notification_active.png",width: 25,height: 25, fit: BoxFit.fitHeight,)),
+                  
+                ],),
+            
+            
+                
+              ],
+            ),
+          ),
+          ),
+      ),
     );
   }
 }
