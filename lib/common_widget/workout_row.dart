ab12d84d2965dc0b565a02679b919acb3cc85b4f
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2025-08-17 22:35:51 +0530

    add workout types in latest Workout part

--- lib/common_widget/workout_row.dart
+++ lib/common_widget/workout_row.dart
@@ -8,13 +8,51 @@ class WorkoutRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
       padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
       decoration: BoxDecoration(
         color: TColor.white,
-        borderRadius: BorderRadius.circular(15),
+        borderRadius: BorderRadius.circular(20),
         boxShadow: const [BoxShadow(color: Colors.black12, blurRadius: 2)],
       ),
-      child: Row(children: []),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Image.asset(
+              wObj["image"].toString(),
+              width: 60,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+          ),
+          
+          const SizedBox(width: 15,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment:CrossAxisAlignment.start,
+              children: [                
+                Text(
+                  wObj["name"].toString(),
+                  style: TextStyle(
+                    color: TColor.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700  ),                
+                ),
+              ],
+            ), ),
+
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(
+              "assets/img/next_icon.png",
+              width: 30,
+              height: 30,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
