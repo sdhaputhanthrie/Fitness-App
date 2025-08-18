77f2c6c978a5f1ae47ffe5c66612efaea365844d
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2025-08-18 20:20:17 +0530

    add details notificationArr

--- lib/view/home/notification_view.dart
+++ lib/view/home/notification_view.dart
@@ -9,6 +9,15 @@ class NotificationView extends StatefulWidget {
 }
 
 class _NotificationViewState extends State<NotificationView> {
+  List notificationArr = [
+    {"image": "assets/img/Workout1.jpg", "title": "Hey, it's time for lunch", "time": "About 1 minutes ago"},
+    {"image": "assets/img/Workout2.jpg", "title": "Don't miss your lowerbody workout", "time": "About 3 hours ago"},
+    {"image": "assets/img/Workout3.jpg", "title": "Hey, let's add some meals for your b", "time": "About 3 hours ago"},
+    {"image": "assets/img/Workout1.jpg", "title": "Congratulations, You have finished A..", "time": "29 May"},
+    {"image": "assets/img/Workout2.jpg", "title": "Hey, it's time for lunch", "time": "8 April"},
+    {"image": "assets/img/Workout3.jpg", "title": "Ups, You have missed your Lowerbo...", "time": "8 April"},
+
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,22 +25,57 @@ class _NotificationViewState extends State<NotificationView> {
         backgroundColor: TColor.white,
         centerTitle: true,
         elevation: 0,
-        leading: Container(         
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Container(
             margin: const EdgeInsets.all(8),
             height: 40,
             width: 40,
             alignment: Alignment.center,
             decoration: BoxDecoration(
-              color: TColor.lightGray,borderRadius: BorderRadius.circular(10)),
-            child: Image.asset("assets/img/black_btn.png", width: 25, height: 25, fit: BoxFit.contain,)),
+              color: TColor.lightGray,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.asset(
+              "assets/img/black_btn.png",
+              width: 25,
+              height: 25,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
 
         title: Text(
           "Notification",
           style: TextStyle(
             color: TColor.black,
             fontSize: 16,
-            fontWeight: FontWeight.w700),
+            fontWeight: FontWeight.w700,
+          ),
         ),
+        actions: [
+          InkWell(
+            onTap: () {},
+            child: Container(
+              margin: const EdgeInsets.all(8),
+              height: 40,
+              width: 40,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: TColor.lightGray,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Image.asset(
+                "assets/img/more-btn.png",
+                width: 25,
+                height: 25,
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+        ],
       ),
       backgroundColor: TColor.white,
     );
