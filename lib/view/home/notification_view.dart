f1141ce23fbdaf36ac97e90dfb92df14844ec0f7
sanadee_2004 <sanadeehaputhanthire@gmail.com>
2025-08-21 23:13:48 +0530

    remove some code lines

--- lib/view/home/notification_view.dart
+++ lib/view/home/notification_view.dart
@@ -1,4 +1,5 @@
 import 'package:fitness/common/colo_extension.dart';
+import 'package:fitness/common_widget/notification_row.dart';
 import 'package:flutter/material.dart';
 
 class NotificationView extends StatefulWidget {
@@ -100,55 +101,13 @@ class _NotificationViewState extends State<NotificationView> {
           ),
         ],
       ),
-      
+
       backgroundColor: TColor.white,
       body: ListView.separated(
         padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
         itemBuilder: ((context, index) {
           var nObj = notificationArr[index] as Map? ?? {};
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8),
-            child: Row(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.asset(
-                    nObj["image"].toString(),
-                    width: 60,
-                    height: 60,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-            
-                const SizedBox(width: 15),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        nObj["title"].toString(),
-                        style: TextStyle(color: TColor.black, fontWeight: FontWeight.w500 , fontSize: 14),
-                      ),
-                      Text(
-                        nObj["time"].toString(),
-                        style: TextStyle(color: TColor.gray, fontSize: 12),
-                      ),
-                    ],
-                  ),
-                ),
-            
-                IconButton(
-                  onPressed: () {},
-                  icon: Image.asset(
-                    "assets/img/sub_menu.png",
-                    width: 30,
-                    height: 30,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ],
-            ),
-          );
+          return NotificationRow(nObj: nObj,);
         }),
         separatorBuilder: (context, index) {
           return Divider(color: TColor.gray.withOpacity(0.5), height: 1);
